f1877c55a52fd4005bfc29bcfaac07d95e36f836
anthony <anthonyarimalala@gmail.com>
2023-06-11 19:17:59 +0300

    sprint4

---
+++ build.bat
@@ -0,0 +1,10 @@
+cd framework/
+    javac -d . *.java
+    jar -cf fw.jar .
+    move fw.jar "D:\ITU\S4\MrNaina\S4_Framework\test-framework\WEB-INF\lib"
+cd ..
+
+cd test-framework/
+    jar -cf test-servlet.war *
+    copy "test-servlet.war" "C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps\test-framework.war"
+cd ..
